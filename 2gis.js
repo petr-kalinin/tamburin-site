d840b85cc7174bc0c0c06a3139207586fc0dd59e
Petr Kalinin <petr@kalinin.nnov.ru>
2016-11-12 22:40:53 +0300

    Better 2gis map

---
+++ 2gis.js
@@ -0,0 +1,13 @@
+var map;
+
+DG.then(function () {
+    map = DG.map('map', {
+        center: [56.321178, 44.012770],
+        zoom: 17
+    });
+    DG.marker([56.321389, 44.013011]).addTo(map).bindPopup('<h6>Тамбурин</h6>студия танца<br/>2 этаж');
+    var options = {vectors: [
+    "LINESTRING(44.012813 56.321074,44.012826 56.321136)",
+    ]};
+    DG.entrance(options).addTo(map).show(true);
+});
