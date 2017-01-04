345928c8102db6d209a772aac6f07311a7a5b6b9
Petr Kalinin <petr@kalinin.nnov.ru>
2017-01-04 22:43:49 +0300

    Major update

--- 2gis.js
+++ 2gis.js
@@ -2,13 +2,15 @@ var map;
 
 DG.then(function () {
     map = DG.map('map', {
-        center: [56.321178, 44.012770],
-        zoom: 17,
+        center: [56.318905, 44.005973],
+        zoom: 15,
         scrollWheelZoom: false
     });
-    DG.marker([56.321389, 44.013011]).addTo(map).bindPopup('<h6>Тамбурин</h6>студия танца<br/>2 этаж');
+    DG.marker([56.321389, 44.013011]).addTo(map).bindPopup('<h6>Нижполиграф</h6>2 этаж');
+    DG.marker([56.317233, 43.999110]).addTo(map).bindPopup('<h6>Звездинка</h6>2 этаж');
     var options = {vectors: [
-    "LINESTRING(44.012813 56.321074,44.012826 56.321136)",
+        "LINESTRING(44.012813 56.321074, 44.012826 56.321136)",
+        "LINESTRING(43.999195 56.317078, 43.999174 56.317132)",
     ]};
-    DG.entrance(options).addTo(map).show(true);
+    DG.entrance(options).addTo(map).show(false);
 });
