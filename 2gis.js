3e12621a50bedb15e205736be8c227ea7db0a443
Petr Kalinin <petr@kalinin.nnov.ru>
2018-09-05 21:55:09 +0300

    Updated for 2018 autumn

--- 2gis.js
+++ 2gis.js
@@ -2,11 +2,10 @@ var map;
 
 DG.then(function () {
     map = DG.map('map', {
-        center: [56.318905, 44.005973],
-        zoom: 15,
+        center: [56.317233, 43.999110],
+        zoom: 16,
         scrollWheelZoom: false
     });
-    DG.marker([56.321389, 44.013011]).addTo(map).bindPopup('<h6>Нижполиграф</h6>2 этаж');
     DG.marker([56.317233, 43.999110]).addTo(map).bindPopup('<h6>Звездинка</h6>2 этаж');
     var options = {vectors: [
         "LINESTRING(44.012813 56.321074, 44.012826 56.321136)",
